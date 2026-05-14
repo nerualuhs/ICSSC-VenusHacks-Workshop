12c404637260fbbeada4ee74c9fa96cf9009cc37
Lauren Shu <113384782+nreualuhs@users.noreply.github.com>
2026-05-13 23:38:12 -0700

    example finished

--- workshop-card/app/components/card.tsx
+++ workshop-card/app/components/card.tsx
@@ -5,22 +5,18 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import StarIcon from '@mui/icons-material/Star';
-import IconButton from '@mui/material/IconButton';
 import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
 
 export default function BasicCard() {
   return (
-    <Card sx={{ minWidth: 320, minHeight: 510, display: 'flex', flexDirection: 'column', padding: 2 }}>
+    <Card sx={{ minWidth: 320, minHeight: 510, display: 'flex', flexDirection: 'column', padding: 2 }} >
       <CardContent>
         <Stack spacing="20px">
         <Box
           className="flex items-center gap-4">
-            <IconButton 
-              sx={{ p: 0 }}>
               <StarIcon 
                 color="secondary" />
-            </IconButton>
             <Typography 
               variant="h1" 
               component="div" 
